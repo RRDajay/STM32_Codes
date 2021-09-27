b8be2a9350796b02f59c4eddc8072ea7f37c3b69
ryan dajay <ryan.rdajay@gmail.com>
2021-09-28 01:40:54 +0800

    template code + blink led

---
+++ blink_led/Core/Src/main.c
@@ -0,0 +1,36 @@
+#include <stdint.h>
+#include "stm32f1xx.h"
+
+#define __HAL_FLASH_PREFETCH_BUFFER_ENABLE()    (FLASH->ACR |= FLASH_ACR_PRFTBE)
+
+void delay(void) {
+	for(uint64_t i = 0; i <= 100000; i++) {
+		asm("nop");
+	}
+}
+
+void init(void) {
+
+	// Configure the Flash prefetch.
+	__HAL_FLASH_PREFETCH_BUFFER_ENABLE();
+
+	// Set Interrupt Group Priority
+	NVIC_SetPriorityGrouping(0x00000003U);
+}
+
+int main(void)
+{
+
+	init();
+
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	GPIOC->CRH |= GPIO_CRH_CNF13_0 | GPIO_CRH_MODE13; // PC13 as output
+	GPIOC->ODR &= (0 << GPIO_ODR_ODR13_Pos); // Initializes PC13 output to LOW
+
+    /* Loop forever */
+	while(1) {
+
+		delay();
+		GPIOC->ODR ^= (1 << GPIO_ODR_ODR13_Pos);
+	}
+}
