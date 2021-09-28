af7591cd59f5fb61bd3a0ec7a657775d90cb43a8
ryan dajay <ryan.rdajay@gmail.com>
2021-09-28 22:39:02 +0800

    changed systick config

--- systick_config/Core/Src/main.c
+++ systick_config/Core/Src/main.c
@@ -1,23 +1,21 @@
-#include <stdint.h>
 #include "stm32f1xx.h"
 #include "config.h"
 
 #define SystemCoreClock 8000000UL;
 
-void delay(void) {
-	for(uint64_t i = 0; i <= 100000; i++) {
-		asm("nop");
-	}
-}
-
 void SysTick_Handler(void) {
+	// Toggle led 
 	GPIOC->ODR ^= (1 << GPIO_ODR_ODR13_Pos);
 }
 
 void systick_init(void) {
+	// Configure Systick Reload Value Register
 	SysTick->LOAD |= SystemCoreClock;
+	// Configure Systick Exception in NVIC
 	NVIC_SetPriority(SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
+	// Configure Systick Current Value Register 
 	SysTick->VAL = 0UL;
+	// Configure Systick Control and Status Register
 	SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_TICKINT_Msk;
 }
 
@@ -37,14 +35,17 @@ int main(void)
 
 	init();
 
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	GPIOC->CRH |= GPIO_CRH_CNF13_0 | GPIO_CRH_MODE13; // PC13 as output
-	GPIOC->ODR &= (0 << GPIO_ODR_ODR13_Pos); // Initializes PC13 output to LOW
+	// Configure APB2 Clock
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; 
+
+	// PC13 as output
+	GPIOC->CRH |= GPIO_CRH_CNF13_0 | GPIO_CRH_MODE13; 
+
+	// Initializes PC13 output to LOW
+	GPIOC->ODR &= (0 << GPIO_ODR_ODR13_Pos);
 
     /* Loop forever */
 	while(1) {
-
-		// delay();
 		
 	}
 }
