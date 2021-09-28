1203f0562a362257253c65e6ca8b220bc9632ffb
ryan dajay <ryan.rdajay@gmail.com>
2021-09-29 02:54:10 +0800

    added RCC config

--- RCC_config/Core/Src/main.c
+++ RCC_config/Core/Src/main.c
@@ -14,14 +14,44 @@ void init(void) {
 	systick_init();
 }
 
+// HSE 8MHz
+void RCC_config(void) {
+
+	// // Configure HSE as MCO, PLL Multiplier (!Must be set before PLL is enabled), PLL entry divider and PLL entry clock source,
+	// // AHB, APB1, APB2 Prescalers and SYSCLK
+	// RCC->CFGR |= RCC_CFGR_MCO_HSE | RCC_CFGR_PLLMULL9_Msk | RCC_CFGR_PLLXTPRE_HSE | 
+	// 			RCC_CFGR_PLLSRC_Msk | RCC_CFGR_PPRE2_DIV1 | RCC_CFGR_PPRE1_DIV2 | RCC_CFGR_HPRE_DIV1 |
+	// 			RCC_CFGR_SWS_PLL | RCC_CFGR_SW_HSI;
+
+	// // Turn on HSE and PLL
+	// RCC->CR |= RCC_CR_HSEON_Msk | RCC_CR_PLLON_Msk;
+
+	// // Configure APB2 Clock
+	// RCC->CR |= (1 << 16);            // Enable HSE
+	// while(!(RCC->CR & (1 << 17)));   // Wait till HSE ready
+	
+	// RCC->CFGR |= 1;                  // Swich to HSE temporarly
+	// RCC->CR   &= ~1;				 // Disable HSI
+	
+	// RCC->CFGR |= (0b111 << 18);      // Set PLL multiplication to 9
+	// RCC->CFGR |= (1 << 16);          // HSE as PLL->SRC
+	
+	// RCC->CR |= (1 << 24);            // Enable PLL
+	// while(!(RCC->CR & (1 << 25)));   // Wait till PLL ready
+	
+	// RCC->CFGR = (RCC->CFGR | 0b10) & ~1; // Set PLL as Clock source
+	// while(!(RCC->CFGR & (1 << 3)));  // Wait till PLL is CLK SRC
+
+}
+
 int main(void)
 {
 
 	init();
+	
+	RCC_config();
 
-	// Configure APB2 Clock
 	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; 
-
 	// PC13 as output
 	GPIOC->CRH |= GPIO_CRH_CNF13_0 | GPIO_CRH_MODE13; 
 
--- RCC_config/Core/Src/systick_init.c
+++ RCC_config/Core/Src/systick_init.c
@@ -1,16 +1,17 @@
 #include "stm32f1xx.h"
 #include "config.h"
 
-#define SystemCoreClock 8000000UL
+#define SystemCoreClock 8000000UL;
 
 void SysTick_Handler(void) {
 	// Toggle led 
-	GPIOC->ODR ^= (1 << GPIO_ODR_ODR13_Pos);
+	// GPIOC->ODR ^= (1 << GPIO_ODR_ODR13_Pos);
+	GPIOC->ODR ^= GPIO_ODR_ODR13_Msk;
 }
 
 void systick_init(void) {
 	// Configure Systick Reload Value Register
-	SysTick->LOAD |= (SystemCoreClock / 1UL);
+	SysTick->LOAD |= SystemCoreClock;
 	// Configure Systick Exception in NVIC
 	NVIC_SetPriority(SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
 	// Configure Systick Current Value Register 
