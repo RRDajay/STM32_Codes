7f0ffefc5aba70766d527b7a667a0bf3d322b39c
ryan dajay <ryan.rdajay@gmail.com>
2021-10-03 03:42:55 +0800

    added gpio driver

--- gpio_driver/Core/Src/main.c
+++ gpio_driver/Core/Src/main.c
@@ -7,8 +7,37 @@ void delay(void) {
 	}
 }
 
+void RCC_config(void) {
+
+	// Enable Prefetch buffer
+	FLASH->ACR |= (0x1UL << FLASH_ACR_PRFTBE_Pos) | FLASH_ACR_LATENCY_1;
+
+	RCC->CR |= RCC_CR_HSEON_Msk;          // Enable HSE
+	while(!(RCC->CR & RCC_CR_HSERDY_Msk));   // Wait till HSE ready
+	
+	RCC->CFGR |= RCC_CFGR_SW_HSE;                 // Swich to HSE temporarly
+	RCC->CR   ^= RCC_CR_HSION_Msk;			 // Disable HSI
+	
+	RCC->CFGR |= RCC_CFGR_PLLMULL9_Msk;      // Set PLL multiplication to 9
+	RCC->CFGR |= RCC_CFGR_PLLSRC_Msk;          // HSE as PLL->SRC
+
+	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1 | RCC_CFGR_PPRE1_DIV2 | RCC_CFGR_HPRE_DIV1;
+
+	RCC->CR |= RCC_CR_PLLON_Msk;            // Enable PLL
+	while(!(RCC->CR & RCC_CR_PLLRDY_Msk));   // Wait till PLL ready
+	
+	RCC->CFGR &= 0xfffffff0; 
+	RCC->CFGR = (RCC->CFGR | RCC_CFGR_SW_PLL); // Set PLL as Clock source
+	while(!(RCC->CFGR & RCC_CFGR_SWS_PLL));  // Wait till PLL is CLK SRC
+
+}
+
 int main(void)
 {
+
+	// RCC Configuration
+	RCC_config();
+
 	// Enable APB2 Clocks for PORTA, PORTB, and PORTC
 	GPIO_CLOCK_ENABLE_PORTA;
 	GPIO_CLOCK_ENABLE_PORTB;
