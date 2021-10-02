9bdc26549b8e0fc38165feee52d2cd68d5f2aa7f
ryan dajay <ryan.rdajay@gmail.com>
2021-10-02 23:51:09 +0800

    added comments

--- blink_led/Core/Src/main.c
+++ blink_led/Core/Src/main.c
@@ -1,18 +1,7 @@
 #include <stdint.h>
 #include "stm32f1xx.h"
 
-#define FLASH_PREFETCH_BUFFER_ENABLE()    (FLASH->ACR |= FLASH_ACR_PRFTBE)
-
-#define NVIC_PRIORITYGROUP_0         0x00000007U /*!< 0 bits for pre-emption priority
-                                                      4 bits for subpriority */
-#define NVIC_PRIORITYGROUP_1         0x00000006U /*!< 1 bits for pre-emption priority
-                                                      3 bits for subpriority */
-#define NVIC_PRIORITYGROUP_2         0x00000005U /*!< 2 bits for pre-emption priority
-                                                      2 bits for subpriority */
-#define NVIC_PRIORITYGROUP_3         0x00000004U /*!< 3 bits for pre-emption priority
-                                                      1 bits for subpriority */
-#define NVIC_PRIORITYGROUP_4         0x00000003U /*!< 4 bits for pre-emption priority
-                                                      0 bits for subpriority */
+#define flash_prefetch_buffer_enable()    (FLASH->ACR |= FLASH_ACR_PRFTBE)
 
 void delay(void) {
 	for(uint64_t i = 0; i <= 100000; i++) {
@@ -23,25 +12,31 @@ void delay(void) {
 void init(void) {
 
 	// Configure the Flash prefetch.
-	FLASH_PREFETCH_BUFFER_ENABLE();
-
-	// Set Interrupt Group Priority
-	NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+	flash_prefetch_buffer_enable();
 }
 
 int main(void)
 {
 
+	// System Init
 	init();
 
+	// Enable APB2 Clock
 	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	GPIOC->CRH |= GPIO_CRH_CNF13_0 | GPIO_CRH_MODE13; // PC13 as output
-	GPIOC->ODR &= (0 << GPIO_ODR_ODR13_Pos); // Initializes PC13 output to LOW
+
+	// PC13 as output
+	GPIOC->CRH |= GPIO_CRH_CNF13_0 | GPIO_CRH_MODE13; 
+	
+	// Initializes PC13 output to LOW
+	GPIOC->ODR &= (0 << GPIO_ODR_ODR13_Pos); 
 
     /* Loop forever */
 	while(1) {
 
 		delay();
+		
+		// Reset LED
 		GPIOC->ODR ^= (1 << GPIO_ODR_ODR13_Pos);
+
 	}
 }
