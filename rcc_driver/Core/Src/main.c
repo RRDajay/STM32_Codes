6ca7c2aa3a7b49b3b1b2e5be144241a962cb9dea
Ryan Dajay <ryan.rdajay@gmail.com>
2021-10-16 16:56:14 +0800

    added rcc driver

--- rcc_driver/Core/Src/main.c
+++ rcc_driver/Core/Src/main.c
@@ -1,40 +1,48 @@
 #include "gpio.h"
 #include "rcc.h"
 #include "systick.h"
-#include "usart.h"
 #include "stm32f1xx.h"
 
-void RCC_config(void) {
+void rcc_config(void) {
 
 	// Enable Prefetch buffer
 	FLASH->ACR |= (0x1UL << FLASH_ACR_PRFTBE_Pos) | FLASH_ACR_LATENCY_1;
 
-	RCC->CR |= RCC_CR_HSEON_Msk;          // Enable HSE
-	while(!(RCC->CR & RCC_CR_HSERDY_Msk));   // Wait till HSE ready
+	// Enable HSE and wait till HSE ready
+	rcc_hse_enable();
 	
-	RCC->CFGR |= RCC_CFGR_SW_HSE;                 // Swich to HSE temporarly
-	RCC->CR   ^= RCC_CR_HSION_Msk;			 // Disable HSI
+	// Swich to HSE temporarly
+	rcc_set_sysclk(HSE_CLK);
+
+	// Disable HSI
+	rcc_hsi_disable();
 	
-	RCC->CFGR |= RCC_CFGR_PLLMULL9_Msk;      // Set PLL multiplication to 9
-	RCC->CFGR |= RCC_CFGR_PLLSRC_Msk;          // HSE as PLL->SRC
-
-	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1 | RCC_CFGR_PPRE1_DIV2 | RCC_CFGR_HPRE_DIV1;
-
-	RCC->CR |= RCC_CR_PLLON_Msk;            // Enable PLL
-	while(!(RCC->CR & RCC_CR_PLLRDY_Msk));   // Wait till PLL ready
+	// Set PLL multiplication to 9
+	rcc_set_pllmul(PLLMUL_9);
 	
-	RCC->CFGR &= 0xfffffff0; 
-	RCC->CFGR = (RCC->CFGR | RCC_CFGR_SW_PLL); // Set PLL as Clock source
-	while(!(RCC->CFGR & RCC_CFGR_SWS_PLL));  // Wait till PLL is CLK SRC
+	// Set HSE as PLL source
+	rcc_set_pllsrc(HSE_CLK);
 
+	// Set APB1, APB2, and AHB Prescalers
+	rcc_set_ppre1_prescaler(DIV_2);
+	rcc_set_ppre2_prescaler(DIV_1);
+	rcc_set_ahb_prescaler(DIV_1);
+
+	// Enable PLL and wait until PLL is ready
+	rcc_pll_enable();
+
+	// Set PLL as Clock source and wait till PLL is CLK source
+	rcc_set_sysclk(PLL_CLK);	
 }
 
 void initialize(void) {
+
 	// RCC Configuration
-	RCC_config();
+	rcc_config();
 
 	// Configure SysTick Timer (1ms tick)
 	systick_init();
+	
 }
 
 int main(void)
@@ -63,6 +71,6 @@ int main(void)
 		
 		__NOP();
 
-		delay_ms(500);
+		delay_ms(1000);
 	}
 }
