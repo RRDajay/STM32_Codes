07dfa9b929d4b25f99c7c12e69022b0f2852ff24
RRDajay <ryan.rdajay@gmail.com>
2021-11-24 21:28:24 +0800

    formatted code

--- Demo/rtos_demo/src/main_mutex.c
+++ Demo/rtos_demo/src/main_mutex.c
@@ -16,9 +16,9 @@
 #if defined(__RTOS__) && defined(__MUTEX_DEMO__)
 
 volatile static uint32_t ulIdleCycleCount;
-void myTask1(void *param);
-void Task2(void *param);
-void Task3(void *param);
+void myTask1(void* param);
+void Task2(void* param);
+void Task3(void* param);
 void vApplicationIdleHook(void);
 SemaphoreHandle_t xMutex;
 
@@ -29,84 +29,88 @@ extern void clkInit(void);
 extern void resetCtrl(void);
 extern void (*usart1_handler)(void);
 
-int main_mutex(void) {
-  // Initializes RCC and FLASH prebuffer
-  clkInit();
+int main_mutex(void)
+{
+    // Initializes RCC and FLASH prebuffer
+    clkInit();
 
-  // Enable apb2 clock for portc and usart1
-  rcc_iopc_clock_enable();
-  rcc_usart1_clock_enable();
-  rcc_iopa_clock_enable();
+    // Enable apb2 clock for portc and usart1
+    rcc_iopc_clock_enable();
+    rcc_usart1_clock_enable();
+    rcc_iopa_clock_enable();
 
-  gpio_config_output_pin(GPIOC, 13, OUTPUT_OD,
-                         S50); // Configure PC13 as output
-  gpio_config_output_pin(GPIOC, 15, OUTPUT_OD,
-                         S50); // Configure PC15 as output
+    gpio_config_output_pin(GPIOC, 13, OUTPUT_OD,
+        S50); // Configure PC13 as output
+    gpio_config_output_pin(GPIOC, 15, OUTPUT_OD,
+        S50); // Configure PC15 as output
 
-  // Configure TX/RX Pins
-  gpio_config_output_pin(GPIOA, 9, OUTPUT_AF_PP, S50);
-  gpio_config_input_pin(GPIOA, 10, INPUT_PU_PD);
+    // Configure TX/RX Pins
+    gpio_config_output_pin(GPIOA, 9, OUTPUT_AF_PP, S50);
+    gpio_config_input_pin(GPIOA, 10, INPUT_PU_PD);
 
-  // Enable USART
-  usart_setup(USART1, 115200);
-  usart1_handler = resetCtrl;
-  usart_enable_irq(USART1);
-  // usart_enable_tcie(USART1);
-  usart_enable_rxneie(USART1);
-  usart_enable(USART1);
+    // Enable USART
+    usart_setup(USART1, 115200);
+    usart1_handler = resetCtrl;
+    usart_enable_irq(USART1);
+    // usart_enable_tcie(USART1);
+    usart_enable_rxneie(USART1);
+    usart_enable(USART1);
 
 #if defined(__RTOS__) && defined(__MUTEX_DEMO__)
 
-  xMutex = xSemaphoreCreateMutex();
-  if (xMutex == NULL) {
-    for (;;)
-      ;
-  }
+    xMutex = xSemaphoreCreateMutex();
+    if (xMutex == NULL) {
+        for (;;)
+            ;
+    }
 
-  xTaskCreate(myTask1, "task1", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
-  xTaskCreate(Task2, "task2", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
-  xTaskCreate(Task3, "task3", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(myTask1, "task1", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(Task2, "task2", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(Task3, "task3", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
 
-  vTaskStartScheduler();
+    vTaskStartScheduler();
 
 #endif
 
-  /* Loop forever */
+    /* Loop forever */
 #if !(defined(__RTOS__))
-  while (1) {
-    gpio_pin_toggle(GPIOC, 13);
-    delay_ms(1000);
-  }
+    while (1) {
+        gpio_pin_toggle(GPIOC, 13);
+        delay_ms(1000);
+    }
 #endif
 }
 
 #if defined(__RTOS__) && defined(__MUTEX_DEMO__)
-void myTask1(void *param) {
-  for (;;) {
-    gpio_pin_toggle(GPIOC, 13);
-    vTaskDelay(250);
-  }
+void myTask1(void* param)
+{
+    for (;;) {
+        gpio_pin_toggle(GPIOC, 13);
+        vTaskDelay(250);
+    }
 }
 
-void Task2(void *param) {
-  for (;;) {
-    if (xSemaphoreTake(xMutex, 10)) {
-      usart_send_string(USART1, "Hello from Task2\r\n");
-      xSemaphoreGive(xMutex);
-    }
+void Task2(void* param)
+{
+    for (;;) {
+        if (xSemaphoreTake(xMutex, 10)) {
+            usart_send_string(USART1, "Hello from Task2\r\n");
+            xSemaphoreGive(xMutex);
+        }
 
-    vTaskDelay(10);
-  }
+        vTaskDelay(10);
+    }
 }
 
-void Task3(void *param) {
-  for (;;) {
-    if (xSemaphoreTake(xMutex, 10)) {
-      xSemaphoreGive(xMutex);
-      usart_send_string(USART1, "Hello from Task3\r\n");
+void Task3(void* param)
+{
+    for (;;) {
+        if (xSemaphoreTake(xMutex, 10)) {
+            xSemaphoreGive(xMutex);
+            usart_send_string(USART1, "Hello from Task3\r\n");
+        }
+        vTaskDelay(10);
     }
-    vTaskDelay(10);
-  }
 }
 
 void vApplicationIdleHook(void) { ulIdleCycleCount++; }
--- Demo/rtos_demo/src/main_software_timers.c
+++ Demo/rtos_demo/src/main_software_timers.c
@@ -24,7 +24,7 @@ void myTimerCallback(TimerHandle_t xTimer);
 void LedBlinkTimerCallback(TimerHandle_t xTimer);
 
 // Task prototypes
-void Task1(void *params);
+void Task1(void* params);
 void vApplicationdIdleHook(void);
 
 #endif
@@ -34,81 +34,82 @@ extern void clkInit(void);
 extern void resetCtrl(void);
 extern void (*usart1_handler)(void);
 
-int main_software_timers(void) {
-  // Initializes RCC and FLASH prebuffer
-  clkInit();
+int main_software_timers(void)
+{
+    // Initializes RCC and FLASH prebuffer
+    clkInit();
 
-  // Enable apb2 clock for portc and usart1
-  rcc_iopc_clock_enable();
-  rcc_usart1_clock_enable();
-  rcc_iopa_clock_enable();
+    // Enable apb2 clock for portc and usart1
+    rcc_iopc_clock_enable();
+    rcc_usart1_clock_enable();
+    rcc_iopa_clock_enable();
 
-  gpio_config_output_pin(GPIOC, 13, OUTPUT_OD,
-                         S50); // Configure PC13 as output
-  gpio_config_output_pin(GPIOC, 15, OUTPUT_OD,
-                         S50); // Configure PC15 as output
+    gpio_config_output_pin(GPIOC, 13, OUTPUT_OD,
+        S50); // Configure PC13 as output
+    gpio_config_output_pin(GPIOC, 15, OUTPUT_OD,
+        S50); // Configure PC15 as output
 
-  // Configure TX/RX Pins
-  gpio_config_output_pin(GPIOA, 9, OUTPUT_AF_PP, S50);
-  gpio_config_input_pin(GPIOA, 10, INPUT_PU_PD);
+    // Configure TX/RX Pins
+    gpio_config_output_pin(GPIOA, 9, OUTPUT_AF_PP, S50);
+    gpio_config_input_pin(GPIOA, 10, INPUT_PU_PD);
 
-  // Enable USART
-  usart_setup(USART1, 115200);
-  usart1_handler = resetCtrl;
-  usart_enable_irq(USART1);
-  // usart_enable_tcie(USART1);
-  usart_enable_rxneie(USART1);
-  usart_enable(USART1);
+    // Enable USART
+    usart_setup(USART1, 115200);
+    usart1_handler = resetCtrl;
+    usart_enable_irq(USART1);
+    // usart_enable_tcie(USART1);
+    usart_enable_rxneie(USART1);
+    usart_enable(USART1);
 
-  usart_send_string(USART1, "Hello from STM32\r\n");
+    usart_send_string(USART1, "Hello from STM32\r\n");
 
 #if defined(__RTOS__) && defined(__SOFTWARE_TIMER_DEMO__)
 
-  xTaskCreate(Task1, "task1", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(Task1, "task1", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
 
-  timer1 =
-      xTimerCreate("One shot timer", 1000, pdTRUE, (void *)0, myTimerCallback);
+    timer1 = xTimerCreate("One shot timer", 1000, pdTRUE, (void*)0, myTimerCallback);
 
-  timer2 =
-      xTimerCreate("timer2", 500, pdTRUE, (void *)1, LedBlinkTimerCallback);
+    timer2 = xTimerCreate("timer2", 500, pdTRUE, (void*)1, LedBlinkTimerCallback);
 
-  if (timer1 == NULL && timer2 == NULL) {
-    usart_send_string(USART1, "Could not initialize timer\r\n");
-    while (1)
-      ;
-  } else {
-    usart_send_string(USART1, "Timers has been initialized\r\n");
-    xTimerStart(timer1, portMAX_DELAY);
-    xTimerStart(timer2, portMAX_DELAY);
-  }
+    if (timer1 == NULL && timer2 == NULL) {
+        usart_send_string(USART1, "Could not initialize timer\r\n");
+        while (1)
+            ;
+    } else {
+        usart_send_string(USART1, "Timers has been initialized\r\n");
+        xTimerStart(timer1, portMAX_DELAY);
+        xTimerStart(timer2, portMAX_DELAY);
+    }
 
-  vTaskStartScheduler();
-  //   vTaskDelete(NULL);
+    vTaskStartScheduler();
+    //   vTaskDelete(NULL);
 
 #endif
 
-  /* Loop forever */
+    /* Loop forever */
 #if !(defined(__RTOS__))
-  while (1) {
-    gpio_pin_toggle(GPIOC, 13);
-    delay_ms(100);
-  }
+    while (1) {
+        gpio_pin_toggle(GPIOC, 13);
+        delay_ms(100);
+    }
 #endif
 }
 
 #if defined(__RTOS__) && defined(__SOFTWARE_TIMER_DEMO__)
 
-void Task1(void *params) {
-  for (;;) {
-    usart_send_string(USART1, "Hello from Task 1\r\n");
-    vTaskDelay(1000);
-  }
+void Task1(void* params)
+{
+    for (;;) {
+        usart_send_string(USART1, "Hello from Task 1\r\n");
+        vTaskDelay(1000);
+    }
 }
 
-void vApplicationIdleHook(void) {}
+void vApplicationIdleHook(void) { }
 
-void myTimerCallback(TimerHandle_t xTimer) {
-  usart_send_string(USART1, "Timer Expired\r\n");
+void myTimerCallback(TimerHandle_t xTimer)
+{
+    usart_send_string(USART1, "Timer Expired\r\n");
 }
 
 void LedBlinkTimerCallback(TimerHandle_t xTimer) { gpio_pin_toggle(GPIOC, 13); }
